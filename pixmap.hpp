96c13dfc6342acaff9111bae36b7a8f9acc935ee
Arcady <arcady@ilyich.(none)>
2007-10-24 23:20:58 -0400

    Add a basic Pixmap class

---
+++ pixmap.hpp
@@ -0,0 +1,16 @@
+#ifndef pixmap_hpp
+#define pixmap_hpp
+
+#include <xcb/xcb.h>
+#include <cairo/cairo.h>
+#include <cairo/cairo-xcb.h>
+
+class Pixmap {
+  xcb_connection_t * conn;
+  xcb_pixmap_t pix_id;
+public:
+  cairo_t * cr;
+  Pixmap(xcb_connection_t *, xcb_screen_t *, int width, int height, int depth);
+};
+
+#endif
